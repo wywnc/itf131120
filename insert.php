3fd82140cb51b85c25cb32d93a7a8d9974de68a7
wywnc <73680478+wywnc@users.noreply.github.com>
2020-11-18 09:21:42 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -12,7 +12,7 @@ if (mysqli_connect_errno($conn))
  $comment = $_POST['comment'];
 
 
-$sql = ""INSERT INTO guestbook (Name, Comment) VALUES ('$name', '$comment')"";
+$sql = "INSERT INTO guestbook (Name, Comment) VALUES ('$name', '$comment')";
 
 
 if (mysqli_query($conn, $sql)) {
