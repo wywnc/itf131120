0574735d1c0a492508c7e2b25ab21cad007dace9
wywnc <73680478+wywnc@users.noreply.github.com>
2020-11-19 16:42:39 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,10 +1,17 @@
-
 <!DOCTYPE html>
 <html>
   <head>
     <title>Deleted</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script> <!-- Datatable -->
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script> <!-- Edit Icon -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
   <body>
     <?php
@@ -20,7 +27,8 @@
     $sql = "DELETE FROM guestbook WHERE id='$id'";
 
     if (mysqli_query($conn, $sql)) {
-        ?><meta http-equiv="refresh" content="time; URL=https://itflab131120.azurewebsites.net/index.php" /></div><?php
+        ?><div class="display-3 text-center mb-3">Deleted Successfully</div>
+        <div class="text-center"><a href="index.php" class="btn btn-warning"><i class="fa fa-home"></i></a></div><?php
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
